bd8f50e05f3f3b5e92a9d5e8cd929d1c5a23e529
santhosh274 <santhoshkumarr.a7@gmail.com>
2025-09-09 19:12:01 +0530

    Update BrainChain.tsx

--- src/pages/events/BrainChain.tsx
+++ src/pages/events/BrainChain.tsx
@@ -181,7 +181,7 @@ const BrainChain = () => {
                   asChild
                 >
                   <a 
-                    href="https://forms.gle/KXUic2FcvPiPySz47" 
+                    href="https://forms.gle/HnewXiLFKE82omNu6" 
                     target="_blank" 
                     rel="noopener noreferrer"
                     className="flex items-center gap-2"
